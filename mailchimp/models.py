6f590b55cd17f346c3b91c5b5136b8ff1506f10b
Imju <imjubyon@gmail.com>
2015-05-30 21:26:52 -0700

    replaced simplejson to json

--- mailchimp/models.py
+++ mailchimp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import simplejson
+from django.utils import json
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.core.urlresolvers import reverse
